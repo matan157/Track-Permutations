43fed59c9a2fd0ae1222a453c36fb0e394845814
Matan Uchen <matan.uchen@gmail.com>
2016-04-14 15:53:05 +0000

    Initial init

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<list>
+using namespace std;
+
+typedef list<string> StringList;
+
+list<string> permute(StringList&);
+void print_list(StringList&);
+int main() {
+    
+    StringList tracks;
+    string track = "";
+    
+    while(getline(cin, track))
+        tracks.push_back(track);
+
+    list<StringList> res = permute(tracks);
+    for(list<StringList>::iterator iter = res.begin(); iter != res.end(); iter++)
+        print_list(*i);
+    
+    return 0;
+}
+
+// Returns a list of lists
+list<StringList> permute(StringList &tracks) {
+    if (tracks.size() == 1)
+        return list<StringList>(1,tracks);
+
+    list<StringList> result;
+    for (StringList::iterator i = tracks.begin(); i != tracks.end(); ) {
+        string temp = (*i);
+        
+        // make a list without the current element
+        StringList tmp(tracks.begin(), i++);
+        tmp.insert(tmp.end(), i, tracks.end());
+
+        // recurse to get all sub-permutations
+        list<StringList> sub = permute(tmp);
+
+        // amend sub-permutations by adding the element
+        for (list<StringList>::iterator j = sub.begin(); j != sub.end(); j++)
+            (*j).push_front(temp);
+
+        result.insert(result.begin(), sub.begin(), sub.end());
+    }
+
+    return result;
+}
+
+void print_list(list<string>& tracks) {
+    list<string>::iterator iter;
+    cout << "========================" << endl;
+    for(iter = tracks.begin(); iter != tracks.end(); iter++)
+        cout << *iter << endl;
+    cout << "=========================" << endl;
+}
